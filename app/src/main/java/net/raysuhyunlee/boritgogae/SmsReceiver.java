2e1977ed610e8cbe130ebbfc255432775a6d9b58
이수현 <SuhyunLee@YasuBook.local>
2015-12-08 04:45:33 +0900

    Add permission to manifest. Create SmsReceiver

---
+++ app/src/main/java/net/raysuhyunlee/boritgogae/SmsReceiver.java
@@ -0,0 +1,15 @@
+package net.raysuhyunlee.boritgogae;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by SuhyunLee on 2015. 12. 8..
+ */
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
