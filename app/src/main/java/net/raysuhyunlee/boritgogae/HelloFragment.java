2d319610152443b9d0e4f6093f19cb49ed7eea32
이수현 <SuhyunLee@YasuBook.local>
2015-12-09 18:55:28 +0900

    Implement HelloFragment(Step 1)

--- app/src/main/java/net/raysuhyunlee/boritgogae/HelloFragment.java
+++ app/src/main/java/net/raysuhyunlee/boritgogae/HelloFragment.java
@@ -4,7 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.snappydb.DB;
+import com.snappydb.DBFactory;
+import com.snappydb.SnappydbException;
+
+import butterknife.Bind;
+import butterknife.BindString;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -13,8 +21,25 @@ import butterknife.OnClick;
  */
 public class HelloFragment extends StepFragment {
 
+    @Bind(R.id.editTextMoney) EditText editTextMoney;
+    @Bind(R.id.textViewError) TextView textViewError;
+    @BindString(R.string.db_key_money_left) String DB_KEY_MONEY_LEFT;
+
     @OnClick(R.id.buttonNext) void next() {
-        onNextEvent.onEvent();
+        String moneyString = editTextMoney.getText().toString();
+        if (moneyString.length() == 0) {
+            textViewError.setText(R.string.error_type_number);
+        } else {
+            try {
+                DB db = DBFactory.open(getContext());
+                int money = Integer.parseInt(moneyString);
+                db.put(DB_KEY_MONEY_LEFT, money);
+                db.close();
+            } catch (SnappydbException e) {
+                e.printStackTrace();
+            }
+            onNextEvent.onEvent();
+        }
     }
 
     @Override
