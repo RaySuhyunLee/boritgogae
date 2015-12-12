321373af01c78ac58bd20fb280187c7f20ffc412
이수현 <SuhyunLee@YasuBook.local>
2015-12-12 16:46:20 +0900

    Resolve #4

--- app/src/main/java/net/raysuhyunlee/boritgogae/MainActivity.java
+++ app/src/main/java/net/raysuhyunlee/boritgogae/MainActivity.java
@@ -1,16 +1,16 @@
 package net.raysuhyunlee.boritgogae;
 
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.SpannableString;
-import android.text.style.RelativeSizeSpan;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.snappydb.DB;
 import com.snappydb.DBFactory;
-import com.snappydb.SnappyDB;
 import com.snappydb.SnappydbException;
 
 import butterknife.Bind;
@@ -39,6 +39,25 @@ public class MainActivity extends AppCompatActivity {
         updateMoneyLeft();
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menuSpendList:
+                openSpendList();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private void updateMoneyLeft() {
         try {
             DB db = DBFactory.open(this);
@@ -48,4 +67,10 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+    private void openSpendList() {
+        Intent intent = new Intent(this, SpendListActivity.class);
+        startActivity(intent);
+    }
+
 }
